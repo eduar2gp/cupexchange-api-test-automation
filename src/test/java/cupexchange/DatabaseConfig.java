c0fc85d2fcca28b996146b4e4d0151bef4fb5bc6
Ed G <eduar2gp@gmail.com>
2026-05-10 16:25:01 -0500

    -market buy and sell orders

--- src/test/java/cupexchange/DatabaseConfig.java
+++ src/test/java/cupexchange/DatabaseConfig.java
@@ -1,26 +1,10 @@
 package cupexchange;
 
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
-public class DatabaseConfig{
-
-    private static final Properties properties = new Properties();
-    static final String config = "config-prod.properties";
-
-    // Load the properties file when the class is first loaded
-    static {
-        try (InputStream input = Files.newInputStream(Paths.get("src/test/resources/"+config))) {
-            properties.load(input);
-        } catch (Exception e) {
-            System.err.println("CRITICAL: Failed to load config.properties. Using default connection values.");
-        }
-    }
+public class DatabaseConfig extends BaseTest{
 
     public static Connection getConnection() throws SQLException {
         // Retrieve values from the properties file with defaults as a fallback
--- src/test/java/cupexchange/ExchangeOrderApiTest.java
+++ src/test/java/cupexchange/ExchangeOrderApiTest.java
@@ -14,12 +14,36 @@ public class ExchangeOrderApiTest extends BaseTest{
 
     @Test
     @Order(1)
-    public void createExchangeOrder() {
+    public void createMarketBuyExchangeOrder() {
         ExchangeOrderRequest request = new ExchangeOrderRequest();
         request.setSide("BUY");
         request.setPairCode("USDCUP");
         request.setType("MARKET");
         request.setVolume(new BigDecimal("10"));
+        request.setUsername(UserVerificationTest.getUserName().toLowerCase());
+
+        String response = given()
+                .header("Authorization", "Bearer " + UserVerificationTest.getJwtToken())
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post(Endpoints.CREATE_EXCHANGE_ORDER)
+                .then()
+                .statusCode(201)
+                .extract()
+                .asString();
+
+        System.out.println(response);
+    }
+
+    @Test
+    @Order(2)
+    public void createMarketSellExchangeOrder() {
+        ExchangeOrderRequest request = new ExchangeOrderRequest();
+        request.setSide("SELL");
+        request.setPairCode("USDCUP");
+        request.setType("MARKET");
+        request.setVolume(new BigDecimal("5"));
         request.setUsername(UserVerificationTest.getUserName().toLowerCase());
 
         String response = given()
